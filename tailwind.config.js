49333d8c6b647c707bcfc57ac18977b9f06a572c
slendyG <josuegarza0608@gmail.com>
2024-09-05 23:15:57 -0700

    working on navbar, set up color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-      primary: {
-        base: '#1a202c',
-        accent: '#0d6efd',
-        text: '#f7fafc',
+    colors: {
+      light: {
+        primary: '#0077B6',
+        secondary: '#00B4D8',
+        accent: '#90E0EF',
+        background: '#F8F9FA',
+        text: '#343A40',
       },
-      secondary: {
-        base: '#212121',
-        accent: '#00ff88',
-        text: '#e0e0e0',
+      dark: {
+        primary: '#0D3B66',
+        secondary: '#FAF0CA',
+        accent: '#F4D35E',
+        background: '#1B263B',
+        text: '#FAF0CA',
       },
+    },
     extend: {},
   },
   plugins: [],
